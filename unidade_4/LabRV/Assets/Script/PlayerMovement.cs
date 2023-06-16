2697210aaada83ae61bf3417b2f22ce0883e80d1
ABarrantesCanales <abarrantescanales@estudiantec.cr>
2023-06-15 18:46:29 -0600

    alterado script de movimento

--- unidade_4/LabRV/Assets/Script/PlayerMovement.cs
+++ unidade_4/LabRV/Assets/Script/PlayerMovement.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float velocity = 10.0f;
-    public float rotation = 90.0f;
+    public float rotationSpeed = 90.0f;
+
+    private float mouseX;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +20,12 @@ public class PlayerMovement : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        float mouseX = Input.GetAxis("Mouse X");
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
 
-        Vector3 dir = new Vector3(x, 0, y) * velocity;
+        Vector3 dir = new Vector3(x, 0, y) * velocity * Time.deltaTime;
 
-        transform.Translate(dir * Time.deltaTime);
+        transform.Translate(dir);
 
-        transform.Rotate(new Vector3(0, mouseX * rotation * Time.deltaTime, 0));
+        transform.rotation = Quaternion.Euler(0, mouseX, 0);
     }
 }
